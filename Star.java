083e6a64a83449ab17585be1be89752fbfbfa66c
Karlsson <karlsson0214@gmail.com>
2020-02-28 12:11:17 +0100

    first

---
+++ Star.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Star here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Star extends Actor
+{
+    /**
+     * Act - do whatever the Star wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        MouseInfo mouse;
+        mouse = Greenfoot.getMouseInfo();
+        if (mouse != null)
+        {
+            setLocation(mouse.getX(), mouse.getY());
+        }
+    }    
+}
